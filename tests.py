f29b4b8acb19d9d2200039384b15a1140940d285
Michał Szymański <smiszym@gmail.com>
2017-03-06 14:14:38 +0100

    Display some info

--- tests.py
+++ tests.py
@@ -2,6 +2,14 @@
 
 import rns
 
+max = 1
+for i in rns.rns_base:
+    max *= i
+max -= 1
+
+print("Base: " + str(rns.rns_base))
+print("Maximum value: " + str(max))
+
 rns.test_rns(4294967300, 4294967200)
 rns.test_rns(4789342294967300, 4294967201557450)
 
