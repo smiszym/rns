7ec7935cdd7d60739900f7cd3b62281f693be26c
Michał Szymański <smiszym@gmail.com>
2017-03-06 17:15:36 +0100

    Skeleton of program main function

--- main.c
+++ main.c
@@ -15,5 +15,34 @@ int main(int argc, char **argv)
 {
         printf("sizeof(struct rns) == %lu\n", sizeof(struct rns));
         printf("sizeof(struct int128) == %lu\n", sizeof(struct int128));
+
+        struct rns rns_a;
+        struct rns rns_b;
+
+        if (argc == 3) {
+                fprintf(stderr, "Reading two decimal numbers: %s and %s\n", argv[1], argv[2]);
+        } else if (argc == 9) {
+                fprintf(stderr, "Reading two numbers in RNS notation\n");
+
+                sscanf(argv[1], "%ul", &rns_a.r0);
+                sscanf(argv[2], "%ul", &rns_a.r1);
+                sscanf(argv[3], "%ul", &rns_a.r2);
+                sscanf(argv[4], "%ul", &rns_a.r3);
+
+                sscanf(argv[5], "%ul", &rns_b.r0);
+                sscanf(argv[6], "%ul", &rns_b.r1);
+                sscanf(argv[7], "%ul", &rns_b.r2);
+                sscanf(argv[8], "%ul", &rns_b.r3);
+
+                fprintf(stderr, "The two numbers are:\n");
+                fprint_rns(stderr, &rns_a);
+                fprint_rns(stderr, &rns_b);
+        } else {
+                fprintf(stderr, "Unknown mode of operation - "
+                                "wrong number of arguments "
+                                "(should be 2 or 8)\n");
+                return 1;
+        }
+
         return 0;
 }
