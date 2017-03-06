f89a64efe1d15cf92bf8164398ef3c5aec425523
Michał Szymański <smiszym@gmail.com>
2017-03-06 18:59:58 +0100

    Remove example Assembler code

--- rns.s
+++ rns.s
@@ -1,23 +1,5 @@
-        .global fib
+        .global rns_add
 
         .text
-fib:
-        movq %rdi, %rcx
-
-        # Case when argument is 1 or 2
-        subq $2, %rcx
-        jg .L2
-        movq $1, %rax
-        ret
-.L2:
-        # %rax contains current Fibonacci number
-        # %rdx contains previous Fibonacci number
-        movq $1, %rax
-        movq $1, %rdx
-.L1:
-        pushq %rax
-        addq %rdx, %rax
-        popq %rdx
-        loop .L1
-
+rns_add:
         ret
