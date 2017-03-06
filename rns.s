8c8dbca32b69bea4ea4c64e4c3b5b3a92a0cbea2
Michał Szymański <smiszym@gmail.com>
2017-03-06 18:58:57 +0100

    Write Assembler implementation of rns_add()

--- rns.s
+++ rns.s
@@ -2,4 +2,50 @@
 
         .text
 rns_add:
+        # %rdi - result address
+        # %rsi - first operand address
+        # %rdx - second operand address
+
+        # TODO modulo
+
+        # r0
+        xorq %rax, %rax
+        movl 0x0(%rsi), %eax
+
+        xorq %rcx, %rcx
+        movl 0x0(%rdx), %ecx
+
+        addq 0x0(%rdx), %rax
+        movl %eax, 0x0(%rdi)
+
+        # r1
+        xorq %rax, %rax
+        movl 0x4(%rsi), %eax
+
+        xorq %rcx, %rcx
+        movl 0x4(%rdx), %ecx
+
+        addq 0x4(%rdx), %rax
+        movl %eax, 0x4(%rdi)
+
+        # r2
+        xorq %rax, %rax
+        movl 0x8(%rsi), %eax
+
+        xorq %rcx, %rcx
+        movl 0x8(%rdx), %ecx
+
+        addq 0x8(%rdx), %rax
+        movl %eax, 0x8(%rdi)
+
+        # r3
+        xorq %rax, %rax
+        movl 0xc(%rsi), %eax
+
+        xorq %rcx, %rcx
+        movl 0xc(%rdx), %ecx
+
+        addq 0xc(%rdx), %rax
+        movl %eax, 0xc(%rdi)
+
         ret
--- utils.c
+++ utils.c
@@ -15,14 +15,6 @@ void fprint_rns(FILE *stream, struct rns *value)
 
 // -- To be later implemented in Assembler
 
-void rns_add(struct rns *result, struct rns *a, struct rns *b)
-{
-        result->r0 = (a->r0 + b->r0) % rns_base.r0;
-        result->r1 = (a->r1 + b->r1) % rns_base.r1;
-        result->r2 = (a->r2 + b->r2) % rns_base.r2;
-        result->r3 = (a->r3 + b->r3) % rns_base.r3;
-}
-
 void rns_sub(struct rns *result, struct rns *a, struct rns *b)
 {
         result->r0 = (a->r0 - b->r0) % rns_base.r0;
