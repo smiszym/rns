250fbe0baea622ca5e79997e1fb89cf558b395f5
Michał Szymański <smiszym@gmail.com>
2017-03-06 17:19:23 +0100

    Implement basic functions in C

--- utils.c
+++ utils.c
@@ -11,3 +11,30 @@ void fprint_rns(FILE *stream, struct rns *value)
 {
         fprintf(stream, "[%d %d %d %d]\n", value->r0, value->r1, value->r2, value->r3);
 }
+
+
+// -- To be later implemented in Assembler
+
+void rns_add(struct rns *result, struct rns *a, struct rns *b)
+{
+        result->r0 = (a->r0 + b->r0) % rns_base.r0;
+        result->r1 = (a->r1 + b->r1) % rns_base.r1;
+        result->r2 = (a->r2 + b->r2) % rns_base.r2;
+        result->r3 = (a->r3 + b->r3) % rns_base.r3;
+}
+
+void rns_sub(struct rns *result, struct rns *a, struct rns *b)
+{
+        result->r0 = (a->r0 - b->r0) % rns_base.r0;
+        result->r1 = (a->r1 - b->r1) % rns_base.r1;
+        result->r2 = (a->r2 - b->r2) % rns_base.r2;
+        result->r3 = (a->r3 - b->r3) % rns_base.r3;
+}
+
+void rns_mul(struct rns *result, struct rns *a, struct rns *b)
+{
+        result->r0 = (a->r0 * b->r0) % rns_base.r0;
+        result->r1 = (a->r1 * b->r1) % rns_base.r1;
+        result->r2 = (a->r2 * b->r2) % rns_base.r2;
+        result->r3 = (a->r3 * b->r3) % rns_base.r3;
+}
