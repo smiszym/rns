b44d4459948d30ec516e420d2a56f15d1ab0da63
Michał Szymański <smiszym@gmail.com>
2017-04-11 14:27:56 +0200

    c_invocation: Add single-arg mode

--- c_invocation.py
+++ c_invocation.py
@@ -8,7 +8,7 @@ def str_to_rns(s):
     return [int(a) for a in s.split(' ')]
 
 
-def invoke_native(num_a, num_b):
+def invoke_native(num_a, num_b=None):
     """
     num_a and num_b should be either Python RNS numbers
     or Python integers.
@@ -26,9 +26,17 @@ def invoke_native(num_a, num_b):
             stdout=PIPE, stderr=PIPE)
         lines = p.communicate()[0].decode('UTF-8').split('\n')
         return [int(s) for s in lines[:-1]]
+    if type(num_a) is int and num_b is None:
+        p = Popen(['./rns', \
+            str(num_a)], \
+            stdout=PIPE, stderr=PIPE)
+        lines = p.communicate()[0].decode('UTF-8').split('\n')
+        return str_to_rns(lines[0])
 
 
 if __name__ == "__main__":
+    print("Launched the module. Performing a self-test.\nCheck manually:")
     print(invoke_native(int_to_rns(2), int_to_rns(4)))
     print(invoke_native([1, 2, 3, 4], [9, 8, 7, 6]))
     print(invoke_native(56, 23))
+    print(invoke_native(35))
