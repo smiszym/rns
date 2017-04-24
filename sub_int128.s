0b2b51ed904a5a13af682c03f6f134b042bb7819
Michał Szymański <smiszym@gmail.com>
2017-04-24 16:38:47 +0200

    Fix sub_int128()

--- sub_int128.s
+++ sub_int128.s
@@ -5,11 +5,11 @@ sub_int128:
         # %rdi - dest address
         # %rsi - src address
 
-        movq $128, %rcx
+        movq $16, %rcx
         clc
 calculating:
-        movb (%rsi), %al
-        movb (%rdi), %dl
+        movb (%rsi), %dl
+        movb (%rdi), %al
         sbbb %dl, %al
         movb %al, (%rdi)
         inc %rsi
