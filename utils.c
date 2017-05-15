73d1be959662703066db1eb05a66c9689414cd83
Michał Szymański <smiszym@gmail.com>
2017-05-15 16:09:46 +0200

    Write rns_to_int() based on newest formula

--- utils.c
+++ utils.c
@@ -25,21 +25,26 @@ void fprint_int128(FILE *stream, struct int128 *value)
 
 void rns_to_int(struct int128 *dest, const struct rns *src)
 {
-        struct int128 tmp;
+        load_r1_to_int128(dest, src);
+        shl_int128(dest, 1);
 
-        load_r0_to_int128(dest, src);
-        shl_int128(dest, 31);
         add_r0_to_int128(dest, src);
-        shl_int128(dest, 61);
+        add_r2_to_int128(dest, src);
+        shl_int128(dest, 30);
+
+        sub_r1_from_int128(dest, src);
+        add_r2_to_int128(dest, src);
+        shl_int128(dest, 1);
+
+        add_r0_to_int128(dest, src);
+        sub_r2_from_int128(dest, src);
+        shl_int128(dest, 30);
+
+        sub_r1_from_int128(dest, src);
+        sub_r2_from_int128(dest, src);
+        shl_int128(dest, 31);
+
         add_r1_to_int128(dest, src);
-        load_r2_to_int128(&tmp, src);
-        shl_int128(&tmp, 92);
-        add_int128(dest, &tmp);
-        load_r1_to_int128(&tmp, src);
-        shl_int128(&tmp, 1);
-        add_r2_to_int128(&tmp, src);
-        shl_int128(&tmp, 61);
-        sub_int128(dest, &tmp);
 }
 
 void int128_to_dec(char *buffer, const struct int128 *number)
